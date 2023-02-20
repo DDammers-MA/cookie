a390052d5cb11496855d4fce2bc92d626459f4d3
Daniel <33257@ma-web.nl>
2023-02-20 22:00:11 +0100

    les 2 af

---
+++ js/main.js
@@ -0,0 +1,154 @@
+class Cookie{
+    name = "";
+    htmlElement = undefined;
+    score = undefined;
+    factor = 1;
+    // dit wordt 1x uitgevoerd wanneer "new" wordt gebruikt
+    constructor(newName, newHTMLElement, newScore){
+        this.name = newName;
+        this.htmlElement = newHTMLElement;
+        this.htmlElement.onclick = this.onCookieClicked;
+        this.score = newScore;
+    }
+
+    onCookieClicked = () =>{
+        this.score.onCookieClicked(this.factor);
+    }
+
+    onStyleChange(){
+        this.htmlElement.classList.add("cookie--chocolate");
+    }
+
+    onStyleChangeLightGreen(){
+        this.htmlElement.classList.add("cookie--LightGreen");
+    }
+}
+
+class Score{
+    score;
+    name = "";
+    htmlElement = undefined;
+
+
+    constructor(newScore, newName, newHTMLElement){
+        this.score = newScore;
+        this.name = newName;
+        this.htmlElement = newHTMLElement;
+        this.htmlElement.innerText = newScore;
+    }
+
+    onCookieClicked(factorFromCookie){
+        this.score = this.score + factorFromCookie;
+        this.htmlElement.innerText = this.score;
+    }
+
+    substractScore(){
+        this.score =  this.score -100;
+        this.htmlElement.innerText = this.score;
+    }
+
+    onAutoScoreClicked(){
+        setInterval( () => {
+            this.score = this.score + 500
+            this.htmlElement.innerText = this.score;
+        }, 10000)
+    }
+
+    addPoints(){
+        this.score = this.score + 10000;
+        this.htmlElement.innerText = this.score;
+    }
+}
+
+class Multiplier{
+    factor = 100;
+    htmlElement = undefined;
+    cookie = undefined;
+    bought = false;
+
+    constructor(htmlElement, cookie){
+        this.htmlElement = htmlElement;
+        this.cookie = cookie;
+        this.htmlElement.onclick = this.onMultiplierClicked;
+    }
+
+    onMultiplierClicked = () =>{
+        if(this.bought === false){
+            this.bought = true;
+        //remove 100 points from score
+        this.cookie.score.substractScore();
+        this.cookie.factor = this.factor;
+        }
+    }
+}
+
+class Autoscore {
+    htmlElement = undefined;
+    score = undefined;
+    bought = false;
+
+    constructor(htmlElement, score){
+        this.htmlElement = htmlElement;
+        this.score = score;
+        this.htmlElement.onclick = this.onAutoScoreClicked;
+    }
+
+    onAutoScoreClicked = () => {
+        if(this.bought === false){
+            this.bought = true;
+            this.score.substractScore();
+            this.score.onAutoScoreClicked();
+        }
+    }
+}
+
+class LightGreenCookie{
+    htmlElement = undefined;
+    bought = false;
+    cookie = undefined
+
+    constructor(htmlElement, cookie){
+        this.htmlElement = htmlElement;
+        this.cookie = cookie;
+        this.htmlElement.onclick = this.onLightGreenCookieClicked;
+    }
+
+    onLightGreenCookieClicked = () =>{
+        if(this.bought === false){
+            this.bought = true;
+            this.cookie.onStyleChangeLightGreen();
+            this.cookie.score.addPoints();
+        }
+    }
+}
+
+class ChocolateCookie{
+    htmlElement = undefined;
+    bought = false;
+    cookie = undefined
+
+    constructor(htmlElement, cookie){
+        this.htmlElement = htmlElement;
+        this.cookie = cookie;
+        this.htmlElement.onclick = this.onChocolateCookieClicked;
+    }
+
+    onChocolateCookieClicked = () =>{
+        if(this.bought === false){
+            this.bought = true;
+            this.cookie.onStyleChange();
+            this.cookie.score.addPoints();
+        }
+    }
+}
+
+const score = new Score(0, "Default score",  document.getElementById("js--score"));
+const cookie = new Cookie("Default Cookie", document.getElementById("js--cookie"), score);
+const multiplier = new Multiplier(document.getElementById("js--multiplier"), cookie);
+const auto = new Autoscore(document.getElementById("js--autoScore"),score);
+const chocolate = new ChocolateCookie(document.getElementById("js--chocolate"), cookie);
+const LightGreen = new LightGreenCookie(document.getElementById("js--LightGreen"), cookie);
+const multiplierMobile = new Multiplier(document.getElementById("js--multiplier--mobile"), cookie);
+const autoMobile = new Autoscore(document.getElementById("js--autoScore--mobile"), score);
+const chocolateMobile = new ChocolateCookie(document.getElementById("js--chocolate--mobile"), cookie);
+const LightGreenMobile = new LightGreenCookie(document.getElementById("js--LightGreen--Mobile"), cookie);
